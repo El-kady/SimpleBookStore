14684b27d016e484ee0308ada149335ebdc5e955
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-04-30 23:22:22 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # SimpleBookStore
 Simple Book Store Using Django Framework
+#Configuration
+Required Settings:
+```
+INSTALLED_APPS += [
+    'SimpleBookStore',
+]
+
+# Template context_processors
+TEMPLATES[0]['OPTIONS']['context_processors'].append("SimpleBookStore.context_processors.simple_book_store_processor")
+
+# Database Example
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': "localhost",
+        'NAME': "django_bookstore",
+        'USER': "root",
+        'PASSWORD': "123456"
+    }
+}
+
+# Media related settings are required for avatar uploading to function properly
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+```
+
+#Configure URLs
+Simply include the urls
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from SimpleBookStore import urls as SimpleBookStore_urls
+
+urlpatterns = [
+    ...
+    url(r'', include(SimpleBookStore_urls, namespace="SimpleBookStore")),
+]
+```
--- tests.py
+++ tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+import unittest
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import random
+import json
+import time
+import os
 
-# Create your tests here.
+driver = webdriver.Chrome()
+
+class MyTest(unittest.TestCase):
+    def setUp(self):
+        driver.get("http://localhost:8800/")
+
+    def test_signup(self):
+        signup_btn = driver.find_element_by_class_name("signup-btn")
+        signup_btn.click()
+
+        usernameInput = driver.find_element_by_id("username")
+        usernameInput.send_keys("Moustafa")
+
+        emailInput = driver.find_element_by_id("email")
+        emailInput.send_keys("Moustafa@gmail.com")
+
+        passwordInput = driver.find_element_by_id("password")
+        passwordInput.send_keys("111111")
+
+        retype_passwordInput = driver.find_element_by_id("retype_password")
+        retype_passwordInput.send_keys("111111")
+
+        retype_passwordInput.send_keys(Keys.RETURN)
+
+        self.assertEqual(driver.current_url, "http://localhost:8800/")
+
+        for btn in driver.find_elements_by_class_name("toggle-follow"):
+            btn.click()
+
+        wait = WebDriverWait(driver, 10)
+        results = wait.until(EC.presence_of_all_elements_located((By.LINK_TEXT, '#duckbar_static>li.zcm__item'),""))
+
+        driver.find_element_by_class_name("header-logo").click()
+
+        for rating in driver.find_elements_by_class_name("rating"):
+            rating.find_element_by_xpath(".//i[3]").click()
+
+    @classmethod
+    def tearDownClass(cls):
+        #driver.quit()
+        pass
+
+
+unittest.main()
